ca9d6f93a5222f1e4a79a8ba267c6ad378038877
kabindrr <chaina_kabindra@yahoo.com>
2024-04-23 16:36:50 +1000

    Final Changes

--- src/config/mongoDbConfig.js
+++ src/config/mongoDbConfig.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectMongo = () => {
-  const uri = "mongodb://localhost:27017/ntdl";
   mongoose
     .connect(process.env.MONGODB_URL)
     .then(() => console.log("DB connected"))
